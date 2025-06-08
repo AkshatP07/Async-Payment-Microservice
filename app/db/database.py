73c189ee4d1bea237b24b43f34ea95dd989b873a
Akshat Parashar <145288745+AkshatP07@users.noreply.github.com>
2025-06-09 00:45:13 +0530

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://neonpost_owner:npg_F2LdPeYTt9JA@ep-orange-smoke-a1rx8t1x-pooler.ap-southeast-1.aws.neon.tech/neonpost?sslmode=require"
+DATABASE_URL = "your db url"
 
 engine = create_engine(DATABASE_URL)
