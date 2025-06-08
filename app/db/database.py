19a0c54781e7759e2c6f085e2509a245679a87af
Akshat Parashar <145288745+AkshatP07@users.noreply.github.com>
2025-06-09 00:46:01 +0530

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 
-DATABASE_URL = "postgresql+asyncpg://neonpost_owner:npg_F2LdPeYTt9JA@ep-orange-smoke-a1rx8t1x-pooler.ap-southeast-1.aws.neon.tech/neonpost"
-
+DATABASE_URL = "postgresql+asyncpg:// your -url in async pg mode"
 engine = create_async_engine(
     DATABASE_URL, 
     echo=True
